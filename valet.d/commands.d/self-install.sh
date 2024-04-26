f9f83f354781e96e6d3f47ae3f163dc8021d5b69
Julien Caillon <julien.caillon@gmail.com>
2024-04-26 18:29:18 +0200

    :rocket: releasing version 0.9.0

--- valet.d/commands.d/self-install.sh
+++ valet.d/commands.d/self-install.sh
@@ -49,7 +49,7 @@ if [[ -z "${BASH_VERSION:-}" ]]; then
 fi
 
 if [[ -z "${VALET_VERSION:-}" ]]; then
-  VALET_VERSION="0.8.14"
+  VALET_VERSION="0.9.0"
 fi
 
 # import the core script (should always be skipped if the command is run from valet)
