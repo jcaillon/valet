db3a7f2c5871e85010c6394fe67e967d1037ab8d
Julien Caillon <julien.caillon@gmail.com>
2024-05-01 23:16:16 +0200

    :rocket: releasing version 0.11.247

--- valet.d/commands.d/self-install.sh
+++ valet.d/commands.d/self-install.sh
@@ -49,7 +49,7 @@ if [[ -z "${BASH_VERSION:-}" ]]; then
 fi
 
 if [[ -z "${VALET_VERSION:-}" ]]; then
-  VALET_VERSION="0.10.10"
+  VALET_VERSION="0.11.247"
 fi
 
 # import the core script (should always be skipped if the command is run from valet)
