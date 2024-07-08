1c8d14aad35e5b02d8c05bfcef87ab6368f42892
Julien Caillon <julien.caillon@gmail.com>
2024-07-08 15:03:13 +0200

    :rocket: releasing version 0.18.426

--- valet.d/commands.d/self-install.sh
+++ valet.d/commands.d/self-install.sh
@@ -650,7 +650,7 @@ function selfUpdate::sourceDependencies() {
 
 # set the version to download by default
 # this is automatically updated by the self release command
-VALET_RELEASED_VERSION="0.17.112"
+VALET_RELEASED_VERSION="0.18.426"
 
 # import the core script (should always be skipped if the command is run from valet)
 if [[ -z "${GLOBAL_CORE_INCLUDED:-}" ]]; then
