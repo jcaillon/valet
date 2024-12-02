3942fbd34dbfec6d443905fb82aa82d1ffcc2094
Julien Caillon <julien.caillon@gmail.com>
2024-12-02 18:15:25 +0100

    :sparkles: added extension.setup.sh

---
+++ extension.setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# This script is sourced by Valet when installing this repository as an extension.
+# It is used to setup the extension and install any dependencies.
+
+log::warning "Setting up extension2..."
