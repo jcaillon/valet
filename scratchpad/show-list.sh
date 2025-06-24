70bc4f41fa9df33a775533d68e9f1a32fa299986
Julien Caillon <julien.caillon@gmail.com>
2025-06-24 11:33:38 +0200

    :construction_worker: lib list

--- scratchpad/show-list.sh
+++ scratchpad/show-list.sh
@@ -7,35 +7,15 @@ include list
 # load the file
 mapfile -t MY_ARRAY < "scratchpad/words"
 
-echo "coucou1"
-tui::createSpace 3
-
-
-echo "${ESC__CURSOR_MOVE__}2${__ESC__DOWN}coucou2"
-tui::createSpace 3
-
-
-echo "${ESC__CURSOR_MOVE__}2${__ESC__DOWN}coucou3"
-tui::createSpace 3
-
-
-echo "${ESC__CURSOR_MOVE__}2${__ESC__DOWN}coucou4"
-tui::createSpace 3
-
-
-echo "${ESC__CURSOR_MOVE__}2${__ESC__DOWN}coucou5"
-tui::createSpace 3
-
-echo "yes"
-
-tui::createSpace 4
-echo "coucou"
-tui::getCursorPosition
-echo "Cursor position: ${GLOBAL_CURSOR_LINE}:${GLOBAL_CURSOR_COLUMN}, ${GLOBAL_LINES}"
-echo "coucou"
-printf "%s" "${ESC__CURSOR_MOVE__}${GLOBAL_CURSOR_LINE};1${__ESC__TO}"
-
 tui::createSpace 9
-echo "yes"
+
+tui::getCursorPosition
+
+_OPTION_TOP=${GLOBAL_CURSOR_LINE} _OPTION_HEIGHT=9 list::setViewport
+list::setItems MY_ARRAY
+list::draw
+# list::filter ""
+
+echo "${ESC__CURSOR_MOVE__}$((GLOBAL_CURSOR_LINE + 8));1${__ESC__TO}"
 
 sleep 1
