64b1f629fca4455ef23e797aa52f59306d7540cd
Julien Caillon <julien.caillon@gmail.com>
2025-06-29 19:40:29 +0200

    :construction_worker: lib-list

--- scratchpad/show-list.sh
+++ scratchpad/show-list.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 export VALET_CONFIG_WARNING_ON_UNEXPECTED_EXIT=false
+export VALET_CONFIG_LOG_FD=tmp/log
+export VALET_CONFIG_LOG_PATTERN="<elapsedTime>{8s} [<pid>{04s}:<subshell>{1s}] <sourceFile>{-5s}:<line>{-4s} <level>{-4s}  <message>"
+
 source "$(valet --source)"
 include list
+
 
 # load the file
 mapfile -t MY_ARRAY < "scratchpad/words"
@@ -24,7 +28,10 @@ MY_ARRAY=(
   "papaya"$'\n'"(orange)"
   "quince"$'\n'"(orange)"
 )
+# MY_ARRAY=(
+# )
 _OPTION_ITEM_HEIGHT=2
+_OPTION_FORCE_SHOW_ITEMS_COUNT=true
 
 tui::createSpace 9
 tui::getCursorPosition
@@ -33,10 +40,9 @@ _OPTION_TOP=${GLOBAL_CURSOR_LINE} _OPTION_HEIGHT=9 list::setViewport
 list::setItems MY_ARRAY
 list::draw
 list::drawCounter
+list::drawScrollBar
 # list::filter ""
 
 echo "${ESC__CURSOR_MOVE__}$((GLOBAL_CURSOR_LINE + 8));1${__ESC__TO}"
 
 sleep 1
-
-# export VALET_CONFIG_LOG_FD=tmp/f
