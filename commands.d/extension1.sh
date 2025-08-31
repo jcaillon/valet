46dcbbfd0ab866b32fb6a4525ce9af11f185ad67
Julien Caillon <julien.caillon@gmail.com>
2025-08-31 20:25:04 +0200

    :sparkles: update valet 0.33

--- commands.d/extension1.sh
+++ commands.d/extension1.sh
@@ -7,19 +7,19 @@ source extension1
 # >>> command: extension1
 #===============================================================
 
-##<<VALET_COMMAND
-# command: extension1
-# function: extension1
-# shortDescription: Do nothing.
-# description: |-
-#   Really, it does nothing.
-# examples:
-# - name: extension1
-#   description: |-
-#     Just run the command and do nothing.
-##VALET_COMMAND
+: <<"COMMAND_YAML"
+command: extension1
+function: extension1
+shortDescription: Do nothing.
+description: |-
+  Really, it does nothing.
+examples:
+- name: extension1
+  description: |-
+    Just run the command and do nothing.
+COMMAND_YAML
 function extension1() {
-  command::parseArguments "$@" && eval "${REPLY}"
+  command::parseArguments "$@"; eval "${REPLY}"
   command::checkParsedResults
 
   extension1::doNothing
