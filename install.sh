1183675ba52ee62d5662f9b6eb31dd60b0e66045
Julien Caillon <julien.caillon@gmail.com>
2026-06-11 18:02:29 +0200

    :rocket: releasing version 0.42.13

--- install.sh
+++ install.sh
@@ -70,7 +70,7 @@ fi
 
 # The version of Valet downloaded by this install script.
 # This value is automatically updated by the `valet self release` command when a new release is published.
-VALET_RELEASED_VERSION="0.41.182"
+VALET_RELEASED_VERSION="0.42.13"
 
 function main() {
   local \
